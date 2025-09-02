8132c2a13bfcac98533067c99f073a94ec370ad7
JihoCho <tryzam@naver.com>
2025-09-02 14:31:29 +0900

    asdf

--- num.py
+++ num.py
@@ -12,11 +12,11 @@ for tc in range(1, 1+t):
     mov = m-n + 1                          # 움직일 수 있는 범위
     maxi = -float('inf')                   # 최댓값 저장
 
-    for j in range(mov):
-        c = 0
-        for k in range(n):
-            c += b[j+k] * a[k]
+    for j in range(mov):                   # 움직이는 거리
+        c = 0                              # 더한 값 저장 변수 선언
+        for k in range(n):                 # 작은 list 크기만큼
+            c += b[j+k] * a[k]             # 마주보는 두 수 곱해서 더함
 
-        maxi = max(maxi, c)
+        maxi = max(maxi, c)                # 더 큰값 저장
  
     print(f"#{tc} {maxi}")
