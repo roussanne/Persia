d820bfc877ff3437a0b2d0083f523d2f638bfb95
JihoCho <tryzam@naver.com>
2025-09-05 01:19:14 +0900

    asdf

---
+++ carrot.py
@@ -0,0 +1,22 @@
+t = int(input().strip())
+
+for tc in range(1, t + 1):
+
+    n = int(input().strip())
+    arr = list(map(int, input().split()))
+
+    max_cnt = 1   # 최소 길이
+    cnt = 1       # 연속 증가 길이
+
+    for i in range(1, n):
+
+        if arr[i] > arr[i-1]:   # 증가하는 경우
+            cnt += 1
+
+            if cnt > max_cnt:   # 최대값 갱신
+                max_cnt = cnt
+
+        else:                   # 증가가 끊기면  reset
+            cnt = 1
+
+    print(f"#{tc} {max_cnt}")
