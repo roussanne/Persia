536eaee7718f3007582f42230d1188d2a6f32f48
JihoCho <tryzam@naver.com>
2025-09-16 11:27:12 +0900

    py

--- ancientone.py
+++ ancientone.py
@@ -1,3 +1,37 @@
 # 9489. 고대 유적
 
-t
+t = int(input())
+
+for tc in range(1, 1 + t):
+
+    n, m = map(int,input().split())
+
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    
+    maxi = 0
+    for i in range(n):
+        cnt = 0
+        for j in range(m):
+
+            if arr[i][j] == 1:
+                cnt += 1
+                maxi = max(cnt, maxi)
+
+            else:
+                cnt = 0
+
+
+    for j in range(m):
+        cnt = 0
+        for i in range(n):
+
+            if arr[i][j] == 1:
+                cnt += 1
+                maxi = max(cnt, maxi)
+
+            else:
+                cnt = 0
+
+
+    print(f'#{tc} {maxi}')
+
--- ancienttwo.py
+++ ancienttwo.py
@@ -1,1 +1,42 @@
 # 20739. 고대 유적 2
+
+# 9489. 고대 유적
+
+t = int(input())
+
+for tc in range(1, 1 + t):
+
+    n, m = map(int,input().split())
+
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    
+    maxi = 0
+    for i in range(n):
+        cnt = 0
+        for j in range(m):
+
+            if arr[i][j] == 1:
+                cnt += 1
+                maxi = max(cnt, maxi)
+
+            else:
+                cnt = 0
+
+
+    for j in range(m):
+        cnt = 0
+        for i in range(n):
+
+            if arr[i][j] == 1:
+                cnt += 1
+                maxi = max(cnt, maxi)
+
+            else:
+                cnt = 0
+
+    
+    if maxi == 1:  # added, if maxi is 1 
+        maxi = 0   # maxi value is invalid
+
+
+    print(f'#{tc} {maxi}')
